701a1e7b62c2d6babf674efea9a7e01c49e404f5
pɹolʇɐƆ <134858054+element10101@users.noreply.github.com>
2023-09-14 10:49:04 -0500

    Rectangle

---
+++ geometry.py
@@ -0,0 +1,42 @@
+class Rectangle:
+  """
+  Represents a rectangle.
+  """
+  def __init__(self, *, l:float=None, w:float=None, a:float=None):
+    """
+    Creates a rectangle.
+
+    @doc [
+      @param l: float -> The length of the rectangle. @keyword
+      @param w: float -> The width of the rectangle. @keyword
+      @param a: float -> The area of the rectangle. @keyword
+    ]
+    """
+    self.l = l
+    self.w = w
+    self.a = a
+  def area(self):
+    """
+    Returns the area of the rectangle.
+
+    @doc [
+      @returns float | None
+      @raises TypeError | None
+    ]
+    """
+    if self.l != None and self.w != None:
+      return self.l * self.w if self.a is None else self.a
+    else:
+      raise TypeError("Either length or width is None")
+  def perimeter(self):
+    """
+    Returns the perimeter of the rectangle.
+
+    @doc [
+      @returns float | None
+      @raises TypeError | None
+    """
+    if self.l != None and self.w != None:
+      return (2 * self.l) + (2 * self.w)
+    else:
+      raise TypeError("Either length or width is None")
